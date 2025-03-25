c92b4e0a752e0a40df3c9091ed195106e153e126
Kgl-01 <karthikgowda.mailme@gmail.com>
2025-03-25 19:15:57 +0530

    control flow using if-else statements

--- bank.go
+++ bank.go
@@ -3,16 +3,28 @@ package main
 import "fmt"
 
 func main() {
+
+	var accountBalance = 1000.0
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What fo you want to do?")
-	fmt.Println("1.Check the balance")
-	fmt.Println("2.Deposit Money")
-	fmt.Println("3.Withdraw Money")
-	fmt.Println("4.Exit")
+	fmt.Println("1. Check the balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit")
 
 	var choice int
 	fmt.Print("Your choice: ")
 	fmt.Scan(&choice)
 
-	fmt.Println("Your choice: ", choice)
+	if choice == 1 {
+		fmt.Println("Your balance is:", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount
+		fmt.Println("Balance updated! New amount:", accountBalance)
+	}
+
 }
