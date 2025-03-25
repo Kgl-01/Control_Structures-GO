589af01c29d947aa701f67ad753fa089739d84de
Kgl-01 <karthikgowda.mailme@gmail.com>
2025-03-25 21:32:36 +0530

    modified .gitIgnore

--- bank.go
+++ bank.go
@@ -14,7 +14,7 @@ func main() {
 	fmt.Println("4. Exit")
 
 	var choice int
-	fmt.Print("Your choice: ")
+	fmt.Print("Enter your choice: ")
 	fmt.Scan(&choice)
 
 	if choice == 1 {
@@ -23,8 +23,35 @@ func main() {
 		fmt.Print("Your deposit: ")
 		var depositAmount float64
 		fmt.Scan(&depositAmount)
+
+		if depositAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0")
+			return
+		}
+
 		accountBalance += depositAmount
 		fmt.Println("Balance updated! New amount:", accountBalance)
+	} else if choice == 3 {
+		fmt.Print("Enter amount:")
+		var withdrawalAmount float64
+		fmt.Scan(&withdrawalAmount)
+
+		if withdrawalAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than zero.")
+			return
+		}
+
+		if withdrawalAmount > accountBalance {
+			fmt.Println("Invalid amount. You can't withdraw more than you have.")
+			return
+		}
+
+		accountBalance -= withdrawalAmount
+		fmt.Println("Withdraw successful! Updated balance:", accountBalance)
+	} else if choice == 4 {
+		fmt.Print("Goodbye!")
+	} else {
+		fmt.Println("Invalid input")
 	}
 
 }
