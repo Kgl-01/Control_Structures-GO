bace674909f3a8462cf18b60cf505389e2440a47
Kgl-01 <karthikgowda.mailme@gmail.com>
2025-03-25 19:00:50 +0530

    initial code of banking console app

--- bank.go
+++ bank.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Banking Application")
+	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("What fo you want to do?")
+	fmt.Println("1.Check the balance")
+	fmt.Println("2.Deposit Money")
+	fmt.Println("3.Withdraw Money")
+	fmt.Println("4.Exit")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	fmt.Println("Your choice: ", choice)
 }
